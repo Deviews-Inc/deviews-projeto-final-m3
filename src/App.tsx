bf0f617236e4658875e1a41b4afd1a330e612560
anavgbc <agomesbc@gmail.com>
2022-08-31 15:12:11 -0300

    :construction: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import GlobalBase from "./styles/base";
 import GlobalReset from "./styles/reset";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <GlobalBase />
+      <Header />
       <GlobalReset />
     </>
   );
