6c865019199a1a1e00f0005a8d454e6d9c6098a0
Emilly <emillymelo3@gmail.com>
2022-09-04 15:25:39 -0300

    :rocket: Adicionando animate no input de pesquisa

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,21 +1,58 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { PostContext } from "../../providers/PostContext";
 import { Container } from "./styles";
+import { motion } from "framer-motion";
+
 const SearchInput = () => {
   const [inputSearchValue, setInputSearchValue] = useState<string>("");
-
   const { searchPost } = useContext(PostContext);
+
+  const [isTablet, setIsTablet] = useState(
+    window.innerWidth >= 768 && window.innerWidth < 1024
+  );
+
+  const updateMedia = () => {
+    setIsTablet(window.innerWidth >= 768 && window.innerWidth < 1024);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
+  const parent = {
+    variantA: { width: 0 },
+    variantB: { width: 100 },
+  };
 
   return (
     <>
       <Container>
-        <input
-          type="text"
-          placeholder="Pesquisar..."
-          value={inputSearchValue}
-          onChange={(event) => setInputSearchValue(event.target.value)}
-        />
+        {isTablet ? (
+          <motion.input
+            type="text"
+            placeholder="Pesquisar..."
+            value={inputSearchValue}
+            onChange={(event) => setInputSearchValue(event.target.value)}
+            variants={parent}
+            initial="variantA"
+            whileFocus="variantB"
+            transition={{
+              type: "spring",
+              damping: 10,
+              mass: 0.5,
+              stiffness: 80,
+            }}
+          />
+        ) : (
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+            value={inputSearchValue}
+            onChange={(event) => setInputSearchValue(event.target.value)}
+          />
+        )}
         <button type="button" onClick={() => searchPost(inputSearchValue)}>
           <AiOutlineSearch />
         </button>
