d4d3de999d7ffd10bae23e7cbbb78089a6bd9ae9
Emilly <emillymelo3@gmail.com>
2022-09-06 14:42:50 -0300

    :rocket: Adicionando input do botão de adicionar imagem

--- src/components/FormPost/styles.ts
+++ src/components/FormPost/styles.ts
@@ -47,24 +47,75 @@ export const Form = styled.form`
 
     .container_buttons {
       display: flex;
-      flex-direction: initial;
+      flex-direction: column;
       justify-content: space-between;
-    }
-
-    button {
       align-items: center;
-      display: flex;
-      justify-content: center;
-      width: 40px;
-      min-height: 1.875rem;
+      align-items: flex-end;
+      gap: 10px;
 
-      svg {
-        height: 60%;
-        width: 70%;
+      @media screen and (min-width: 425px) {
+        flex-direction: initial;
+      }
+
+      .container_add_img {
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+        min-height: 1.875rem;
+        border-radius: 0.3em;
+        padding: 0 2px;
+
+        background: linear-gradient(var(--button-grad-red));
+
+        box-shadow: var(--box-shadow);
+
+        button {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          width: 40px;
+          min-height: 1.875rem;
+          padding: 5px;
+
+          border-radius: 0.25rem;
+          background-color: transparent;
+          color: var(--color-white-0);
+
+          &:hover {
+            background-color: rgba(211, 0, 0, 0.83);
+          }
+
+          svg {
+            height: 60%;
+            width: 70%;
+          }
+
+          @media screen and (min-width: 425px) {
+            min-width: 40px;
+          }
+        }
+
+        input {
+          min-height: 28px;
+          width: 100%;
+          height: 90%;
+          background-color: var(--color-grey-4);
+
+          text-align: center;
+          font-size: 0.875rem;
+          outline: 0;
+
+          border-radius: 4px;
+          color: var(--color-white-0);
+        }
+
+        @media screen and (min-width: 425px) {
+          flex-direction: row;
+        }
       }
     }
 
-    button + button {
+    button {
       align-self: flex-end;
       min-height: 1.875rem;
       width: 3.75rem;
